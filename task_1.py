cef551ac85b6f4770cd1d7e6ca317910b57c60ff
Alyona Gavrylei <Alyona@Mac-mini-Alyona.local>
2016-11-01 11:10:14 +0200

    tasks 1, 2, 3 done

---
+++ task_1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+a = int(input())
+n = "Silence is golden"
+for i in range(a):
+	print(n)
---
+++ task_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+number = 998
+while number < 1025:
+	number += 3
+	print(number, end = ' ')
